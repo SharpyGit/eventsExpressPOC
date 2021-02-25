aba9023c08994c2beb59d2d116a220bee9cacc3c
DavidSharp <david.sharp@nationwide.co.uk>
2021-02-25 17:49:54 +0000

    users router created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8081;
 
 const eventsRouter = require("./routes/event-router")
+const usersRouter = require("./routes/user-router")
 
 app.use(express.json())
 
@@ -10,18 +11,5 @@ app.listen(port, () => {console.log(`Application listening on http://localhost:$
 
 app.get("/", (req, res) => {res.send("Successful get at top level index.js")})
 app.use("/events", eventsRouter)
-
-app.post("/", (req, res) => {
-    
-    console.log(`req: `, req.body)
-    const name = req.body.name
-    const password = req.body.password
-
-    if(name.toLowerCase() == "david" && password == "password123") {
-        res.status(200).send({message: "Welcome David", authenticated: true})
-    }
-    else {
-        res.status(401).send({message: "User not found, who are you", authenticated: false})
-    }
-})
+app.use("/users", usersRouter) 
 
