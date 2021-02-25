7cb45ba3b53fb812681d34f008f85a1cb3ebe4f4
DavidSharp <david.sharp@nationwide.co.uk>
2021-02-25 17:34:01 +0000

    created post request

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
-
 const app = express();
-
 const port = 8081;
+
+app.use(express.json())
 
 app.listen(port, () => {console.log(`Application listening on http://localhost:${port}`)})
 
-app.get("/", (req, res) => {res.send("Any string basically, HELLO WORLD!!!!")})
+app.get("/", (req, res) => {res.send("Middleware is working")})
+
+app.post("/", (req, res) => {
+    
+    console.log(`req: `, req.body)
+    const name = req.body.name
+    const password = req.body.password
+
+    if(name.toLowerCase() == "david" && password == "password123") {
+        res.status(200).send({message: "Welcome David", authenticated: true})
+    }
+    else {
+        res.status(401).send({message: "User not found, who are you", authenticated: false})
+    }
+})
+
